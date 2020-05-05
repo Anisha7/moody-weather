f3754d3ee3a3bf9f8c0392d95727d907512faaa7
Anisha Jain <anishajain@outlook.com>
2020-05-05 10:20:57 -0700

    styles and such

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,28 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { getWeatherByZip, getCityFromZip } from "../../helpers/apis";
 import "./styles.css";
 
 const Home = () => {
   const [zip, setZip] = useState("");
   const [display, setDisplay] = useState(false);
+  const [scroll, setScroll] = useState(false);
   const [location, setLocation] = useState("");
   const [iconUrl, setIconUrl] = useState("");
   const [description, setDescription] = useState("");
   const [windSpeed, setWindSpeed] = useState(0);
+  
+  const displayRef = useRef(null);
+
   const displayWeather = (json) => {
     // do something
     console.log(json);
     const weather = json.weather[0];
     // update location prop
-    getCityFromZip(zip, setLocation);
+    setLocation(`${json.name} ${zip}`)
     // other info
     setIconUrl(`http://openweathermap.org/img/wn/${weather.icon}@2x.png`);
     setDescription(weather.description);
     setWindSpeed(json.wind.speed);
     // get emojis set
     setDisplay(true);
+    setScroll(true);
+    setZip("")
   };
 
+  if (scroll === true ) {
+    window.scrollTo({
+        top: displayRef.current.offsetTop,
+        left: 0,
+        behavior: 'smooth'
+      })
+    setScroll(false);
+  }
   return (
     <div>
       <h1>Moody Weather</h1>
@@ -41,9 +55,9 @@ const Home = () => {
       <button onClick={() => getWeatherByZip(zip, displayWeather)}>
         Submit
       </button>
-      {display ? <div>
+      {display ? <div ref={displayRef} className="display">
           <h2>{location}</h2>
-          <img src={iconUrl} alt="weather icon" />
+          <img className="weather-icon" src={iconUrl} alt="weather icon" />
           <h3>{description}</h3>
           <p>Wind speed: {windSpeed}</p>
       </div> : ""}
--- src/helpers/apis.js
+++ src/helpers/apis.js
@@ -10,23 +10,3 @@ export const getWeatherByZip = async (zipCode, callback) => {
     .then((json) => callback(json))
     .catch((err) => console.log(err.message));
 };
-
-export const getCityFromZip = async (zipCode, callback) => {
-  const key = process.env.REACT_APP_ZIPCODE_API_KEY;
-  console.log(key);
-  fetch(
-    `https://www.zipcodeapi.com/rest/${key}/info.json/${zipCode}/degrees`,
-    {
-      method: "GET",
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
-    // After a connection is made the data is streamed as JSON
-    .then((res) => res.json())
-    // When that resolves we use `this.setState()` to assign the weather data to state which will also re-render the component.
-    .then((json) => callback(`${json.city}, ${json.state} ${zipCode}`))
-    .catch((err) => console.log(err.message));
-};
